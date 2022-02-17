76d7e2e7d48e34d5963967e7a1268774c1b62658
Dhanraj <catalystwalls@gmail.com>
2022-02-17 18:48:22 +0530

    Revert "test"
    This reverts commit f851286e95917473a4c36d7d22636c361339a1be.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-import 'dart:io';
-
-import 'package:flutter_play_asset_delivery/flutter_play_asset_delivery.dart';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,12 +29,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<File>? image;
-  Future<File>? image2;
-  Future<File>? image3;
-  Future<File>? image4;
-  Future<File>? image5;
-  Future<File>? image6;
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,124 +44,23 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: ListView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextButton(
-                onPressed: () {
-                  image = FlutterPlayAssetDelivery.getAssetFile(
-                      "A_Better_Weather.kwgt.zip");
-                },
-                child: const Text('1')),
-            FutureBuilder(
-              future: image,
-              builder: (_, AsyncSnapshot<File> snapshot) {
-                if (!snapshot.hasData) return const CircularProgressIndicator();
-                if (!snapshot.hasError) {
-                  return const CircularProgressIndicator(
-                    color: Colors.red,
-                    value: 1,
-                  );
-                }
-                return const Text('dn');
-              },
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            TextButton(
-                onPressed: () {
-                  image2 = FlutterPlayAssetDelivery.getAssetFile(
-                      "kuper_calendar.kwgt");
-                },
-                child: const Text('2')),
-            FutureBuilder(
-              future: image2,
-              builder: (_, AsyncSnapshot<File> snapshot) {
-                if (!snapshot.hasData) return const CircularProgressIndicator();
-                if (!snapshot.hasError) {
-                  return const CircularProgressIndicator(
-                    color: Colors.red,
-                    value: 1,
-                  );
-                }
-                return const Text('dn');
-              },
-            ),
-            TextButton(
-                onPressed: () {
-                  image3 =
-                      FlutterPlayAssetDelivery.getAssetFile("kuper_music.kwgt");
-                },
-                child: const Text('3')),
-            FutureBuilder(
-              future: image3,
-              builder: (_, AsyncSnapshot<File> snapshot) {
-                if (!snapshot.hasData) return const CircularProgressIndicator();
-                if (!snapshot.hasError) {
-                  return const CircularProgressIndicator(
-                    color: Colors.red,
-                    value: 1,
-                  );
-                }
-                return const Text('dn');
-              },
-            ),
-            TextButton(
-                onPressed: () {
-                  image4 =
-                      FlutterPlayAssetDelivery.getAssetFile("kuper_strip.kwgt");
-                },
-                child: const Text('4')),
-            FutureBuilder(
-              future: image4,
-              builder: (_, AsyncSnapshot<File> snapshot) {
-                if (!snapshot.hasData) return const CircularProgressIndicator();
-                if (!snapshot.hasError) {
-                  return const CircularProgressIndicator(
-                    color: Colors.red,
-                    value: 1,
-                  );
-                }
-                return const Text('dn');
-              },
-            ),
-            TextButton(
-                onPressed: () {
-                  image5 =
-                      FlutterPlayAssetDelivery.getAssetFile("kuper_type.kwgt");
-                },
-                child: const Text('5')),
-            FutureBuilder(
-              future: image5,
-              builder: (_, AsyncSnapshot<File> snapshot) {
-                if (!snapshot.hasData) return const CircularProgressIndicator();
-                if (!snapshot.hasError) {
-                  return const CircularProgressIndicator(
-                    color: Colors.red,
-                    value: 1,
-                  );
-                }
-                return const Text('dn');
-              },
-            ),
-            TextButton(
-                onPressed: () {
-                  image6 = FlutterPlayAssetDelivery.getAssetFile(
-                      "kuper_unread_counter.kwgt");
-                },
-                child: const Text('6')),
-            FutureBuilder(
-              future: image6,
-              builder: (_, AsyncSnapshot<File> snapshot) {
-                if (!snapshot.hasData) return const CircularProgressIndicator();
-                if (!snapshot.hasError) {
-                  return const CircularProgressIndicator(
-                    color: Colors.red,
-                    value: 1,
-                  );
-                }
-                return const Text('dn');
-              },
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
